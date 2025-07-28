4cfaa31350798eadf7a8abb358f80c3dd0a5d021
fanxt <3515228784@qq.com>
2025-07-28 15:48:21 +0800

    更新README.md
    Signed-off-by: fanxt <3515228784@qq.com>

--- local_helper_client/src/main/resources/static/js/settings.js
+++ local_helper_client/src/main/resources/static/js/settings.js
@@ -255,7 +255,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="about-container">
                 <header class="about-header">
                     <h1 class="gradient-title">关于我们</h1>
-                    <p class="version-tag">Version 2.4.4</p>
+                    <p class="version-tag">Version 2.4.5</p>
                 </header>
                 
                 <div class="info-grid">
